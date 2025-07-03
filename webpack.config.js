3131864de8f3ba8366b85c5c39eb06f3f7b99490
Alexmig24 <alexandermqc2004@hotmail.com>
2025-07-02 19:57:54 -0500

    Update de las rutas dentro del webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin=require('html-webpack-plugin');
-// const { Templateemplate } = require('webpack');
 module.exports={
     mode: 'development',
     devtool: 'source-map',
-    entry: './index.js',
+    entry: './product_grid/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
